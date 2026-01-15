755774096981929955b991de93992e7c7f5255bb
Spicy - musicenter <zerikarliodam@gmail.com>
2026-01-16 00:13:31 +0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ import "./Header.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 // import logo from "../../assets/logo.png";
-import infoLogo from "../../netlivylogo.png";
+import netlivyLogo from "../../netlivylogo.png";
 const Header = () => {
     const [show, setShow] = useState("top");
     const [lastScrollY, setLastScrollY] = useState(0);
@@ -78,7 +78,7 @@ const Header = () => {
             <ContentWrapper>
                 <div className="cent"> 
                <div className="logo" onClick={()=> navigate("/")}>
-   <img src={netlivylogoLogo} alt="Logo" />
+   <img src={netlivyLogo} alt="Logo" />
 </div>
 
                 <ul className="menuItems">
